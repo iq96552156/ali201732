ff48bef14aaf45bd3185036f7167436c1ddebf10
chenshuju <chenshuju_2@126.com>
2017-03-02 21:30:37 +0800

    1.0.0.3

--- dahuaxiyou2/share.js
+++ dahuaxiyou2/share.js
@@ -3,12 +3,12 @@ var sharedata = {
 	//    以下有注释的部分可按求作修改
 
     'title':'《大梦西游2铁扇公主》VIP免费看!!',//微信群配置(标题)
-    'link':'https://chenshuju.github.io/tv/dahuaxiyou2/index.html',//分享到群的链接
+    'link':'https://iq96552156.github.io/ali201732/dahuaxiyou2/index.html',//分享到群的链接
     'imgUrl':'https://chenshuju.github.io/xxoo02/static/images/73ac16eb8b7ae36d48fecf3555fabfff_256_256.png',//分享到群的连接上的图片
     'desc':'爱奇艺《大梦西游2铁扇公主》免会员去广告直接观看。播放次数:32098768次',//分享到群的文字
 
     'qtitle':'爱奇艺新版《大梦西游2铁扇公主》免费看',//朋友圈配置
-    'qlink':'https://chenshuju.github.io/tv/dahuaxiyou2/index.html',//分享到朋友圈的链接
+    'qlink':'https://iq96552156.github.io/ali201732/dahuaxiyou2/index.html',//分享到朋友圈的链接
     'qimgUrl':'https://chenshuju.github.io/xxoo02/static/images/73ac16eb8b7ae36d48fecf3555fabfff_256_256.png',//分享到朋友圈的链接上的图片
     'qdesc':'爱奇艺《大梦西游2铁扇公主》免会员去广告直接观看。播放次数:32098768次',//分享到朋友圈的链接的标题
     'success':function(res){
--- shediaoyingxiong/share.js
+++ shediaoyingxiong/share.js
@@ -3,12 +3,12 @@ var sharedata = {
 	//    以下有注释的部分可按求作修改
 
     'title':'《射雕英雄传》VIP免费看!!',//微信群配置(标题)
-    'link':'https://chenshuju.github.io/tv/shediaoyingxiong/index.html',//分享到群的链接
+    'link':'https://iq96552156.github.io/ali201732/shediaoyingxiong/index.html',//分享到群的链接
     'imgUrl':'https://chenshuju.github.io/xxoo02/static/images/73ac16eb8b7ae36d48fecf3555fabfff_256_256.png',//分享到群的连接上的图片
     'desc':'爱奇艺《射雕英雄传》免会员去广告直接观看。播放次数:32098768次',//分享到群的文字
 
     'qtitle':'爱奇艺新版《射雕英雄传》免费看',//朋友圈配置
-    'qlink':'https://chenshuju.github.io/tv/shediaoyingxiong/index.html',//分享到朋友圈的链接
+    'qlink':'https://iq96552156.github.io/ali201732/shediaoyingxiong/index.html',//分享到朋友圈的链接
     'qimgUrl':'https://chenshuju.github.io/xxoo02/static/images/73ac16eb8b7ae36d48fecf3555fabfff_256_256.png',//分享到朋友圈的链接上的图片
     'qdesc':'爱奇艺《射雕英雄传》免会员去广告直接观看。播放次数:32098768次',//分享到朋友圈的链接的标题
     'success':function(res){
