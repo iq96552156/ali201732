f7eb386e0ef25d707dc3cb3d257d9fbad5b1c795
iq96552156 <iq96552156@163.com>
2017-03-16 15:47:55 +0800

    1.1.1.1.1
    111111

--- jixiantegong2/film.js
+++
@@ -1,6 +0,0 @@
-var film_url = new Array(
-                "http://www.iqiyi.com/19rrak7l6c.html"
-	);
-
-var transforurl = "http://yun.mt2t.com/yun?url=";
-var playurl = transforurl + film_url[0];
--- xuezhangangjulin/film.js
+++
@@ -1,6 +0,0 @@
-var film_url = new Array(
-                "http://www.iqiyi.com/v_19rr9ts93w.html"
-	);
-
-var transforurl = "http://yun.mt2t.com/yun?url=";
-var playurl = transforurl + film_url[0];
